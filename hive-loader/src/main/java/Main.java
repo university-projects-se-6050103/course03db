9777d2a0d5de5c71e1a9f8bb5f2475a0a8e57892
Vlad Holubiev <golubev.vld@gmail.com>
2017-05-28 11:56:32 +0300

    Finish java script to load seed hive data

--- hive-loader/src/main/java/Main.java
+++ hive-loader/src/main/java/Main.java
@@ -1,6 +1,5 @@
 import java.sql.SQLException;
 import java.sql.Connection;
-import java.sql.ResultSet;
 import java.sql.Statement;
 import java.sql.DriverManager;
 
@@ -16,47 +15,18 @@ public class Main {
         }
 
         Connection con = DriverManager.getConnection("jdbc:hive2://localhost:10000/default", "hive", "cloudera");
-        String tableName = "kkk";
-
         Statement stmt = con.createStatement();
-        stmt.execute("drop table if exists " + tableName);
-        stmt.execute("CREATE TABLE IF NOT EXISTS kkk( id int, test_id int, student_id int, value int ) row format delimited fields terminated by \",”;");
 
-        // show tables
-        String sql = "show tables '" + tableName + "'";
-        System.out.println("Running: " + sql);
+        stmt.execute("drop table if exists scores");
+        stmt.execute("CREATE TABLE IF NOT EXISTS scores( id int, test_id int, student_id int, value int ) row format delimited fields terminated by \",\"");
+        stmt.execute("LOAD DATA INPATH 'hdfs:///user/cloudera/data/scores.csv' OVERWRITE INTO TABLE scores");
 
-        ResultSet res = stmt.executeQuery(sql);
-        if (res.next()) {
-            System.out.println(res.getString(1));
-        }
+        stmt.execute("drop table if exists students");
+        stmt.execute("CREATE TABLE IF NOT EXISTS students( id int, full_name varchar(250), group varchar(10), study_year int ) row format delimited fields terminated by \",\"");
+        stmt.execute("LOAD DATA INPATH 'hdfs:///user/cloudera/data/students.csv' OVERWRITE INTO TABLE students");
 
-        sql = "describe " + tableName;
-        System.out.println("Running: " + sql);
-
-        res = stmt.executeQuery(sql);
-        while (res.next()) {
-            System.out.println(res.getString(1) + "\t" + res.getString(2));
-        }
-
-        sql = "LOAD DATA INPATH 'hdfs:///user/cloudera/data/scores.csv' OVERWRITE INTO TABLE kkk;";
-        System.out.println("Running: " + sql);
-        stmt.execute(sql);
-
-        sql = "select * from " + tableName;
-        System.out.println("Running: " + sql);
-
-        res = stmt.executeQuery(sql);
-        while (res.next()) {
-            System.out.println(String.valueOf(res.getInt(1)) + "\t" + res.getString(2));
-        }
-
-        sql = "select count(1) from " + tableName;
-        System.out.println("Running: " + sql);
-
-        res = stmt.executeQuery(sql);
-        while (res.next()) {
-            System.out.println(res.getString(1));
-        }
+        stmt.execute("drop table if exists subjects");
+        stmt.execute("CREATE TABLE IF NOT EXISTS subjects( id int, name varchar(250), teacher_id int ) row format delimited fields terminated by \",\"");
+        stmt.execute("LOAD DATA INPATH 'hdfs:///user/cloudera/data/subjects.csv' OVERWRITE INTO TABLE subjects");
     }
 }
